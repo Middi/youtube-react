02fb820626b90d47174235cfe693cdd7a862535b
“Richard Middleton” <Richard@richardmiddleton.me>
2018-06-09 00:22:19 +0200

    fixed from deleted video

--- src/components/video-item/index.js
+++ src/components/video-item/index.js
@@ -4,11 +4,11 @@ import './style.css';
 class VideoItem extends Component {
     render() {
         let video = this.props.video;
-        // let img = this.props.video.thumbnails.medium.url;
+        let img = this.props.video.thumbnails.medium.url;
         return (
                 <article onClick={ () => this.props.itemClicked(video.resourceId.videoId)}>
                     <div className="thumb-container">
-                        {/* <img src={img} className="thumb" alt="Video Thumbnail" /> */}
+                        <img src={img} className="thumb" alt="Video Thumbnail" />
                     </div>
                     <div className="vid-details">
                         <h1>{video.title}</h1>
